bae37b85a2e7f379e2ccecb1e15173332d0d5f67
abdulWadood63 <121860097+abdulWadood63@users.noreply.github.com>
2023-04-29 12:58:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,6 @@ pattern = "*.mp3"
 
 mixer.init()
 
-# prev_img = tk.PhotoImage(file= "prev_img.png")
-# stop_img = tk.PhotoImage(file= 'stop_img.png')
-# play_img = tk.PhotoImage(file= 'play_img.png')
-# pause_img = tk.PhotoImage(file= 'pause_img.png')
-# next_img = tk.PhotoImage(file= 'next_img.png')
-
 def select():
     label.config(text= listBox.get('anchor'))
     mixer.music.load(rootpath + "\\" + listBox.get('anchor'))
@@ -72,10 +66,6 @@ label.pack(pady=15)
 top=tk.Frame(canvas,bg ='black')
 top.pack(padx=10,pady=5,anchor = 'center')
 
-
-
-
-
 prevButton = tk.Button(canvas,text="previous" , command= play_prev,bg = 'blue')
 prevButton.pack(pady=15, in_ = top ,side ='left')
 
@@ -90,22 +80,6 @@ stopButton.pack(pady=15, in_ = top ,side ='left')
 
 nextButton = tk.Button(canvas,text="Next",command= play_next,bg = "blue")
 nextButton.pack(pady=15, in_ = top ,side ='left')
-
-# prevButton = tk.Button(canvas,text="prev", image= prev_img, bg = "black", borderwidth =0, command= play_prev)
-# prevButton.pack(pady=15, in_ = top ,side ='left')
-
-# pauseButton = tk.Button(canvas,text="Pause", image= pause_img, bg = "black", borderwidth =0, command= pause_song )
-# pauseButton.pack(pady=15, in_ = top ,side ='left')
-
-# playButton = tk.Button(canvas,text="Play", image= play_img, bg = "black", borderwidth =0,command= select)
-# playButton.pack(pady=15, in_ = top ,side ='left')
-
-# stopButton = tk.Button(canvas,text="Stop", image= stop_img, bg = "black", borderwidth =0,command= stop)
-# stopButton.pack(pady=15, in_ = top ,side ='left')
-
-# nextButton = tk.Button(canvas,text="Next", image= next_img, bg = "black", borderwidth =0,command= play_next)
-# nextButton.pack(pady=15, in_ = top ,side ='left')
-
 
 for root,dirs, files in os.walk(rootpath):
     for filename in fnmatch.filter(files,pattern):
